4a9df5598c6c35d23500804827e68f7a2f62c661
Carlos Araújo <carlosaaraujopc@gmail.com>
2023-07-30 18:03:20 -0300

    fix: handle unexpected data with exceptions

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('TODO API')
