22e0b403d464aff6f842a264cfb7e7a0140a41e7
Carlos Araújo <carlosaaraujopc@gmail.com>
2023-07-30 01:29:50 -0300

    feat: create database communication in todo service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRespository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.todoRespository.find();
+  }
+
+  async findOneOrFail(id: string) {
+    try {
+      return await this.todoRespository.findOneOrFail({ where: { id: id } });
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  async create(data: CreateTodoDto) {
+    return await this.todoRespository.save(this.todoRespository.create(data));
+  }
+
+  async update(id: string, data: UpdateTodoDto) {
+    const todo = await this.findOneOrFail(id);
+
+    this.todoRespository.merge(todo, data);
+    return await this.todoRespository.save(todo);
+  }
+
+  async deleteById(id: string) {
+    await this.findOneOrFail(id);
+    await this.todoRespository.softDelete(id);
+  }
+}
